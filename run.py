0e504e7b4d1970e248e5900fb4b236db0f07f8da
mattyu <mattyu@tencent.com>
2017-08-08 10:40:23 +0800

    添加脚本运行示例。run.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@
 from twisted.internet import reactor,defer
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
+from prePushSpider.spiders.KanDianArticleSpider import KanDianArticleSpider
 
+setting = get_project_settings()
 runner = CrawlerRunner(get_project_settings())
 
 # 'followall' is the name of one of the spiders of the project.
@@ -11,7 +13,7 @@ runner = CrawlerRunner(get_project_settings())
 
 @defer.inlineCallbacks
 def crawl():
-    yield runner.crawl('KanDianArticleSpider')
+    yield runner.crawl(KanDianArticleSpider)
     yield runner.crawl('UrlSpider')
     reactor.stop()
 
